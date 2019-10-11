7f89957f4b5eb07c3811181ed09fa0b63e34f0e1
yxwk <2932085012@qq.com>
2019-10-11 23:55:11 +0800

    第二次

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require("gulp");
 
 gulp.task("copyallfile",async ()=>{
-	gulp.src("./**/*").pipe(gulp.dest("D:\\phpStudy\\WWW\\DT"));
+	gulp.src("./**/*").pipe(gulp.dest("d:\\phpStudy\\WWW\\DT"));
 });
 
 gulp.task("watchall",async ()=>{
-	gulp.src("*.html")
-	.pipe(gulp.dest("D:\\phpStudy\\WWW\\DT"));
+	gulp.watch("*.html",async ()=>{
+		gulp.src("*.html")
+		.pipe(gulp.dest("d:\\phpStudy\\WWW\\DT"));
+	});
 });
